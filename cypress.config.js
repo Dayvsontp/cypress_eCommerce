2b3d1fbed37c17d25721a90087b8768ad689c79f
Dayvson Tavares <dayvsontp@hotmail.com>
2025-09-16 16:29:42 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,37 +4,50 @@ const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = defineConfig({
   e2e: {
-     e2e: {
-    baseUrl: 'http://www.automationpractice.pl/index.php?',
+    e2e: {
+      baseUrl: 'http://www.automationpractice.pl/index.php?',
     },
     videosFolder: 'cypress/videos',
     video: true,
     setupNodeEvents(on, config) {
-      const options = {
-        webpackOptions: {
-          resolve: {
-            extensions: ['.ts', '.js']
-          },
-          module: {
-            rules: [
-              {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-              }
-            ]
+      if (config.env.noSandbox === "true") {
+        config.browsers = config.browsers.map((browser) => {
+          if (browser.name === 'chrome' || browser.name === 'chromium') {
+            return {
+              ...browser,
+              customLaunchArgs: ['--no-sandbox']
+            }
           }
-        },
-      };
+          return browser
+        })
+      }
+          const options = {
+            webpackOptions: {
+              resolve: {
+                extensions: ['.ts', '.js']
+              },
+              module: {
+                rules: [
+                  {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: ['@babel/preset-env']
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+          };
 
-      on('file:preprocessor', webpack(options));
-      allureWriter(on, config);
-      return config;
-    },
+          on('file:preprocessor', webpack(options));
+          allureWriter(on, config);
+          return config;
+        },
   },
-});
+    });
+
+
